b161a34cddf7f2acbc8592d594d9d0857f8dc727
Tobias Bladh <tobias.bladh@mindified.com>
2024-12-08 14:48:40 +0100

    day 8 solved

--- solutions/year_2024/day8.py
+++ solutions/year_2024/day8.py
@@ -1,4 +1,5 @@
 import itertools as it
+import math
 
 def get_antennas(input) -> dict:
     antennas = dict()
@@ -23,6 +24,8 @@ def get_antinodes(antennas):
            antinodes.append((b[0] - x_diff, b[1] - y_diff))
     return antinodes
 
+
+
 def in_field(max_x, max_y, node):
     return (
         0 <= node[0] < max_x and
@@ -39,4 +42,34 @@ def part1(input):
    in_field_antinodes = filter(lambda node: in_field(max_x, max_y, node), antinodes)
    return len(set(in_field_antinodes))
 
+def part2(input):
+    max_y = len(input.splitlines())
+    max_x = len(input.splitlines()[0]) 
+    in_field_lambda = lambda node: in_field(max_x, max_y, node)
+
+    def get_harmonics_antinodes(antennas):
+        antinodes = set()
+        for antennas in antennas.values():
+            for pair in it.permutations(antennas, 2):
+                a = pair[0]
+                b = pair[1]
+
+                antinodes.update(harmonics_line(a, b))
+        return antinodes
+                
+
+    def harmonics_line(a, b):
+        x_diff = a[0] - b[0]
+        y_diff = a[1] - b[1]
+        lgd = math.gcd(x_diff, y_diff)
+        x_diff, y_diff = x_diff / lgd, y_diff / lgd
+
+        above = it.takewhile(in_field_lambda, zip(it.count(a[0], x_diff), it.count(a[1], y_diff)))
+        below = it.takewhile(in_field_lambda, zip(it.count(a[0], -x_diff), it.count(a[1], -y_diff)))
+
+        return set(above).union(set(below))
+
+    antennas = get_antennas(input)
+    antinodes = get_harmonics_antinodes(antennas)
+    return len(antinodes)
 
